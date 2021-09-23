69e1d3b4e9238f9d582053c2d384a4a2b61a50fd
kira1214 <muhammadyusuf87p@gmail.com>
2021-09-23 10:36:17 +0700

    upload file CI3 dan contoh1.php

---
+++ pustaka-booking/application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class contoh1 extends CI_controller
+{
+
+    public function index()
+    {
+        echo"<h1>perkenalkan</h1>";
+        echo"Nama saya Muhammad yusuf permana
+        saya tinggal di daerah leuwisadeng
+        saya sangat ingin mengerti dan memahami
+        matkul ini dan yang lainnya";
+    }
+}
