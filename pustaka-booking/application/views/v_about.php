78a686e0c92f6523211963e5971f96f23954e65a
kira1214 <muhammadyusuf87p@gmail.com>
2021-10-09 15:56:58 +0700

    membuat template sederhana dan dinamis dan setting base_url().

---
+++ pustaka-booking/application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan      : Muhammad </li>
+        <li>Nama Belakang   : Yusuf Permana
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Cagak Desa Kalong 1 Kec.leuwisadeng Kab.BOGOR</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>BOGOR</li>
+    </ul>
+    
+    <h4>Olaha Raga Favorite</h4>
+    <ul type="square">
+        <li>BASKET</li>
+        <li>JOGING</li>
+    </ul>
+</section>
